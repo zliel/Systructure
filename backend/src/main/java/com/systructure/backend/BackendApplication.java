9d5fad221f2dfad1fc934b5e955d0182caa5f838
zliel <47030040+zliel@users.noreply.github.com>
2026-01-21 14:49:34 -0800

    Ensured the com.systructure package is scanned for components

--- backend/src/main/java/com/systructure/backend/BackendApplication.java
+++ backend/src/main/java/com/systructure/backend/BackendApplication.java
@@ -2,12 +2,12 @@ package com.systructure.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.systructure"})
 public class BackendApplication {
-
-	static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }

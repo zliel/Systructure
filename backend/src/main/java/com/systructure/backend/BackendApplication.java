c74a3e0e02cce0e2a575d41e1e437c663f53f889
zliel <47030040+zliel@users.noreply.github.com>
2026-01-21 12:08:15 -0800

    Backend initial commit

---
+++ backend/src/main/java/com/systructure/backend/BackendApplication.java
@@ -0,0 +1,13 @@
+package com.systructure.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}

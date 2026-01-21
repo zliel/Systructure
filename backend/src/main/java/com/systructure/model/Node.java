6ba8799e2ec82ca4119bb545553165be96c29576
zliel <47030040+zliel@users.noreply.github.com>
2026-01-21 14:47:59 -0800

    Added a node type with some dummy data

---
+++ backend/src/main/java/com/systructure/model/Node.java
@@ -0,0 +1,19 @@
+package com.systructure.model;
+
+import java.util.Arrays;
+import java.util.List;
+
+public record Node(String id, NodeType type, float xPos, float yPos) {
+    public static List<Node> nodes = Arrays.asList(
+            new Node("1", NodeType.SERVICE, 100.0f, 150.0f),
+            new Node("2", NodeType.DATABASE, 200.0f, 250.0f),
+            new Node("3", NodeType.GATEWAY, 300.0f, 350.0f)
+    );
+
+    public static Node getById(String id) {
+        return nodes.stream()
+                .filter(node -> node.id().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}

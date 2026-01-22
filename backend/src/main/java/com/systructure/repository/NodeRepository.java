54e54ad5b080344135bec0d182def8ebd315ef5f
zliel <47030040+zliel@users.noreply.github.com>
2026-01-22 14:25:57 -0800

    Updated repositories to use Longs specifically

--- backend/src/main/java/com/systructure/repository/NodeRepository.java
+++ backend/src/main/java/com/systructure/repository/NodeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface NodeRepository extends JpaRepository<Node, Integer> {
+public interface NodeRepository extends JpaRepository<Node, Long> {
 }

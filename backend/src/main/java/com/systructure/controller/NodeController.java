44603d358c6dc437711e137cd9b443cfe7877f2f
zliel <47030040+zliel@users.noreply.github.com>
2026-01-22 09:46:13 -0800

    Added  repositories for the types

--- backend/src/main/java/com/systructure/controller/NodeController.java
+++ backend/src/main/java/com/systructure/controller/NodeController.java
@@ -1,14 +1,21 @@
 package com.systructure.controller;
 
+import com.systructure.model.Node;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
-import com.systructure.model.Node;
+
+import java.util.List;
 
 @Controller
 public class NodeController {
     @QueryMapping
-    public Node nodeById(@Argument String id) {
+    public Node nodeById(@Argument Long id) {
         return Node.getById(id);
+    }
+
+    @QueryMapping
+    public List<Node> allNodes() {
+        return Node.nodes;
     }
 }
---
+++ backend/src/main/java/com/systructure/repository/NodeRepository.java
@@ -0,0 +1,7 @@
+package com.systructure.repository;
+
+import com.systructure.model.Node;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NodeRepository extends JpaRepository<Node, Integer> {
+}

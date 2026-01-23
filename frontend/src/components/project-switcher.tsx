fac72a9691d102ba09426d4cb87cef3f21248796
zliel <47030040+zliel@users.noreply.github.com>
2026-01-23 15:42:28 -0800

    Removed the project plan from the project-switcher

--- frontend/src/components/project-switcher.tsx
+++ frontend/src/components/project-switcher.tsx
@@ -46,7 +46,6 @@ export function ProjectSwitcher({
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">{activeProject.name}</span>
-                <span className="truncate text-xs">{activeProject.plan}</span>
               </div>
               <ChevronsUpDown className="ml-auto" />
             </SidebarMenuButton>

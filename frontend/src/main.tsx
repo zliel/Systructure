f3cb689ff7f378f415882e154f85c540697d3e67
zliel <47030040+zliel@users.noreply.github.com>
2026-01-24 10:28:31 -0800

    Set up the query provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,22 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.tsx"
 import { SidebarProvider } from "./components/ui/sidebar.tsx"
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
+import { ApolloProvider } from "@apollo/client/react"
+
+const apolloClient = new ApolloClient({
+  link: new HttpLink({
+    uri: import.meta.env.VITE_GRAPHQL_ENDPOINT as string,
+  }),
+  cache: new InMemoryCache(),
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SidebarProvider>
-      <App />
-    </SidebarProvider>
+    <ApolloProvider client={apolloClient}>
+      <SidebarProvider>
+        <App />
+      </SidebarProvider>
+    </ApolloProvider>
   </StrictMode >
 )

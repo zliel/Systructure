870d0a47e0195535d3f5f9a832769a3dd6ae46a8
zliel <47030040+zliel@users.noreply.github.com>
2026-01-23 16:54:48 -0800

    Minor visual update, added a rin around the project logo in the project switcher

--- frontend/src/components/project-switcher.tsx
+++ frontend/src/components/project-switcher.tsx
@@ -41,7 +41,7 @@ export function ProjectSwitcher({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+              <div className="bg-sidebar-primary text-sidebar-primary-foreground border border-sidebar-accent-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
                 <activeProject.logo className="size-4" />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">

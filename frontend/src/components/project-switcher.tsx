0930da80efb61de7ea3502929c3b6f7f3b28915c
zliel <47030040+zliel@users.noreply.github.com>
2026-01-25 11:41:17 -0800

    Added a separate border color for the profile logo when in dark mode

--- frontend/src/components/project-switcher.tsx
+++ frontend/src/components/project-switcher.tsx
@@ -40,7 +40,7 @@ export function ProjectSwitcher({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <div className="bg-sidebar-primary text-sidebar-primary-foreground border border-sidebar-accent-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
+              <div className="bg-sidebar-primary text-sidebar-primary-foreground border border-sidebar-accent-foreground dark:border-gray-900 flex aspect-square size-8 items-center justify-center rounded-lg">
                 <Languages size="4" />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">

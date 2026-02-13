a97dac55f9c798583d33e41d62752dd9109d5dee
zliel <47030040+zliel@users.noreply.github.com>
2026-02-13 09:46:43 -0800

    Moved queries and mutations to related features and split between features

---
+++ frontend/src/features/projects/api/queries.ts
@@ -0,0 +1,48 @@
+import { gql } from '@apollo/client';
+
+export const GET_USER = gql`
+  query GetUser($userId: ID!) {
+    userById(id: $userId) {
+      id
+      username
+      email
+      role
+      projectMemberships {
+        id
+        projectRole
+        joinedAt
+        project {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
+export const GET_USER_PROJECTS = gql`
+  query GetUserProjects($userId: ID!) {
+    userById(id: $userId) {
+      projectMemberships {
+        id
+        projectRole
+        joinedAt
+        project {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
+export const CREATE_PROJECT = gql`
+  mutation CreateProject($input: CreateProjectInput!) {
+    createProject(newProjectData: $input) {
+      id
+      name
+      description
+      isPublic
+    }
+  }
+`;

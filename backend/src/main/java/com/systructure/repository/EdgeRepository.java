c265a816bdc03f0ad04ab840308ca8770958a5ab
zliel <47030040+zliel@users.noreply.github.com>
2026-02-19 11:47:28 -0800

    Added some methods to the repositories for fetching items in batches

--- backend/src/main/java/com/systructure/repository/EdgeRepository.java
+++ backend/src/main/java/com/systructure/repository/EdgeRepository.java
@@ -1,10 +1,15 @@
 package com.systructure.repository;
 
 import com.systructure.model.Edge;
+import com.systructure.model.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface EdgeRepository extends JpaRepository<Edge, Long> {
     public void deleteBySourceNodeIdOrTargetNodeId(Long sourceNode_id, Long targetNode_id);
+
+    List<Edge> findByProjectIn(List<Project> projects);
 }
